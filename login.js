a8b7b30c8d222ae442c238add5169959380a0efc
Damian <damian@thepuzzlers.co>
2017-11-15 13:52:08 +0100

    dom manipulation

--- login.js
+++ login.js
@@ -2,25 +2,19 @@
 
 function login(userInputEmail, userInputPassword) {
     
-        //
+    
         for (let userCount = 0; userCount < databaseUsers.length; userCount++) {
-            //login successful if email and password are in database
-
+    
             if (userInputEmail === databaseUsers[userCount].email && userInputPassword === databaseUsers[userCount].password) {
                 console.log('We logged you in');
-
-                userInputName = databaseUsers[userCount].first_name;
-                userInputLastName = databaseUsers[userCount].last_name;
-                //userInputEmail = userInputEmail (leave out because already defined; only way to get to this func)
-                userID = databaseUsers[userCount]._id.$oid;
-
-                console.log(userInputName, userInputLastName, userID);
-
-            //error if password/email is wrong
+                // console.log(databaseUsers[userCount]._id.$oid);
+            
             } else if (userInputEmail === databaseUsers[userCount].email) {
                 console.log(`Sry wrong password`);  
             } else {
                 console.log(`Sry we don't know ${userInputEmail}`);  
             } 
         }
+
+        document.getElementById("form-stage").innerHTML = `<h1>Logged In</h1>`;
 }
